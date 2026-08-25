4083b44c7ccb1e0176992e9c206c9a277f918549
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-26 00:30:49 +0500

    Update get_ip.sh
    Add check of utils. if utils is don't installed then we download it!

--- get_ip.sh
+++ get_ip.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+if ! command -v dig &> /dev/null; then
+    echo "dig is not found. install it..."
+    sudo apt-get update && sudo apt-get install -y dnsutils
+fi
+
 domains=("google.com" "github.com" "youtube.com" "discord.com" "soundcloud.com")
+
 for domain in "${domains[@]}"; do
   ip=$(dig +short "$domain" | head -n1)
   echo "$domain —> $ip"
