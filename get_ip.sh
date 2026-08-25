22c20910bf78e93dae5166d467402b053efadf55
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-26 00:17:09 +0500

    Update get_ip.sh
    made the first version ip-getter

--- get_ip.sh
+++ get_ip.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Here will be my bash file. It'll be get your IP"
+domains=("google.com" "github.com" "youtube.com" "discord.com" "soundcloud.com")
+for domain in "${domains[@]}"; do
+  ip=$(dig +short "$domain" | head -n1)
+  echo "$domain —> $ip"
+done
