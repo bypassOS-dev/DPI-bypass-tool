0f41dd0fe2abc179b70695ca9b994a34022a4b5c
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-26 22:18:57 +0500

    Update get_ip.sh

--- get_ip.sh
+++ get_ip.sh
@@ -4,10 +4,24 @@
 LIST_URL="https://raw.githubusercontent.com/runetfreedom/russia-blocked-geosite/main/data/community.lst"
 LIST_FILE="knows_ip.txt"
 LIST_IP="result.txt"
-
+AGE_HOURS=6
+#===Check of the dig===
 if ! command -v dig &> /dev/null; then
     echo "dig is not found. install it..."
     sudo apt-get update && sudo apt-get install -y dnsutils
+fi
+#===Is the list of domain old?===
+need_download=0
+if [! -f "$LIST_FILE"]; then
+    #It's mean that file doesn't exist. Download:
+    need_download=1
+else
+    #The file is exist. Check his age:
+    file_age=$(($(date +%s) - $(stat -c %Y "LIST_FILE")))
+    AGE_SECOND=$((AGE_HOURS * 3600))
+    if ["file_age" -gt "AGE_SECOND"]; then
+        need_download=1
+    fi
 fi
 
 for domain in "${domains[@]}"; do
