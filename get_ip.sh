15e181f9bf5489ee11fe1cb43d92f3da6f214c4d
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-26 21:57:28 +0500

    Update get_ip.sh

--- get_ip.sh
+++ get_ip.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+#===Get all blocked domain. for example in russia ===
+LIST_URL="https://raw.githubusercontent.com/runetfreedom/russia-blocked-geosite/main/data/community.lst"
+LIST_FILE="knows_ip.txt"
+LIST_IP="result.txt"
+
 if ! command -v dig &> /dev/null; then
     echo "dig is not found. install it..."
     sudo apt-get update && sudo apt-get install -y dnsutils
 fi
-
-domains=("google.com" "github.com" "youtube.com" "discord.com" "soundcloud.com")
 
 for domain in "${domains[@]}"; do
   ip=$(dig +short "$domain" | head -n1)
