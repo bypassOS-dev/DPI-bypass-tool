4a1a01d9ae66712ed9e335f7e8809564ee3e2088
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-29 11:26:27 +0500

    Add coments

--- src/fragmenting/fake_ttl.rs
+++ src/fragmenting/fake_ttl.rs
@@ -15,26 +15,27 @@ pub fn send_fake_ttl(
     ttl: u8,
     random_text: &[u8]
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let (mut tx, _) = transport_channel(2048, Layer3(IpNextHeaderProtocols::Tcp))?;
+    let (mut tx, _) = transport_channel(2048, Layer3(IpNextHeaderProtocols::Tcp))?; // Open a special communication channel  
 
         //Calculate total lenght of TCP-segments in bytes:
        // 20 bytes: 
       // 4 bytes: sender's port (2 bytes) + recipient's port (2 bytes)
      // 10 bytes: Sequence Numbet (4 bytes) + Acknowledgement number (4 bytes) + flags and header lenght (2 bytes)
     // 6 bytes: Window Size (2 bytes) + Checksum (2 bytes) + Urgent Pointer (2 bytes) 
-    let tcp_len = 20 + random_text.len();
+    let tcp_len = 20 + random_text.len(); //calculate how many bytes the entire TCP-segment will be occupy
     let mut tcp_buf = vec![0u8; tcp_len];
-    let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buf)
-        .ok_or("Failed to create TCP packet buffer")?;
+    let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buf) // Create a "wrapper-helper"
+        .ok_or("Failed to create TCP packet buffer")?;  // If buffer is too small then program end work
 
-    tcp_packet.set_source(my_ip.port());
-    tcp_packet.set_destination(server_ip.port());
-    tcp_packet.set_sequence(seq);
-    tcp_packet.set_acknowledgement(ack);
-    tcp_packet.set_flags(TcpFlags::ACK | TcpFlags::PSH);
-    tcp_packet.set_window(64240);
-    tcp_packet.set_data_offset(5);
-    tcp_packet.set_payload(random_text);
+    // Start to fill our "wrapper" 
+    tcp_packet.set_source(my_ip.port());                  // Write to TCP-header sender's port
+    tcp_packet.set_destination(server_ip.port());         // Write to TCP-header recipient's port
+    tcp_packet.set_sequence(seq);                         // Write to TCP-header sequence number
+    tcp_packet.set_acknowledgement(ack);                  // Write to TCP-header acknowledgement numbet 
+    tcp_packet.set_flags(TcpFlags::ACK | TcpFlags::PSH);  // A combination of flags that usualy found in normal packet
+    tcp_packet.set_window(64240);                         // Write to TCP-header standart window size
+    tcp_packet.set_data_offset(5);                        // Say to recipient where start payload (5 piece of 4 bytes)
+    tcp_packet.set_payload(random_text);                 // Write payload after TCP-header (20 bytes)
 
     let checksum = tcp::ipv4_checksum(&tcp_packet.to_immutable(), my_ip.ip(), server_ip.ip());
     tcp_packet.set_checksum(checksum);
