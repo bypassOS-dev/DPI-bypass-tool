dd8d336de376f5d78467506cf9a23a4b9482f199
bypassOS-dev <265340571+bypassOS-dev@users.noreply.github.com>
2026-09-13 11:32:57 +0500

    debugging...

--- src/net_filter_queue.rs
+++ src/net_filter_queue.rs
@@ -12,7 +12,7 @@ pub fn start_sniff() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut queue = Queue::open()?;
     queue.bind(0)?;
 
-    let mut sequence_storage: Vec<u32> = Vec::new();
+    let mut pending: Option<(u32, Vec<u8>)> = None;
 
     loop {
         // "Catching" all packets
@@ -28,14 +28,74 @@ pub fn start_sniff() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         if let Some(ip_packet) = Ipv4Packet::new(payload) {
             if let Some(tcp_packet) = TcpPacket::new(ip_packet.payload()) {
                 let seq = tcp_packet.get_sequence();
-
-                // Get tcp-payload and just do simple output
+                // Get tcp-payload
                 let tcp_payload = tcp_packet.payload();
-                println!(
-                    "Packet: seq: {}, tcp payload lenght: {}",
-                    tcp_packet.get_sequence(),
-                    tcp_payload.len()
-                );
+
+                if let Some((pending_seq, pending_data)) = &mut pending {
+                    let expected_seq = pending_seq.wrapping_add(pending_data.len() as u32);
+                    if seq == expected_seq {
+                        pending_data.extend_from_slice(tcp_payload);
+                        println!("Glued! Total len now: {}", pending_data.len());
+
+                        // "find-sni" - it is my function.
+                        // You can read her code!
+                        // In a nutshell this function return 2 values:
+                        // Split posicion and damain
+                        if let Some((split_pos, domain)) = find_sni(pending_data) {
+                            println!("Found SNI: {} (split at {})", domain, split_pos);
+
+                            // Generate random value of junk data
+                            let mut rng = thread_rng();
+                            let trash = rng.gen_range(10..=30);
+
+                            // Recognize sequence number this packet
+                            let real_seq = tcp_packet.get_sequence();
+
+                            // Create vector that will be populated
+                            // letters "A". There will be our random num of "A"
+                            let junk: Vec<u8> = vec![0x41; trash];
+                            let mut packet1_payload = junk.clone();
+
+                            // We add a piece of real packet
+                            // to our junk (we gum up it in simple words)
+                            packet1_payload.extend_from_slice(&tcp_payload[..split_pos]);
+
+                            // Calculate sequence number for our false packet
+                            let packet1_seq = real_seq.wrapping_sub(trash as u32);
+
+                            let packet2_payload = &tcp_payload[split_pos..];
+
+                            let packet2_seq = real_seq + split_pos as u32;
+
+                            println!(
+                                "Packet 1: seq={}, len={}",
+                                packet1_seq,
+                                packet1_payload.len()
+                            );
+                            println!(
+                                "Packet 2: seq={}, len={}",
+                                packet2_seq,
+                                packet2_payload.len()
+                            );
+
+                            let my_ip =
+                                SocketAddrV4::new(ip_packet.get_source(), tcp_packet.get_source());
+                            let dst_ip = SocketAddrV4::new(
+                                ip_packet.get_destination(),
+                                tcp_packet.get_destination(),
+                            );
+                            let ack = tcp_packet.get_acknowledgement();
+
+                            send_fake_ttl(my_ip, dst_ip, packet1_seq, ack, 64, &packet1_payload)?;
+                            send_fake_ttl(my_ip, dst_ip, packet2_seq, ack, 64, packet2_payload)?;
+
+                            pending = None;
+                        }
+                        msg.set_verdict(Verdict::Drop);
+                        queue.verdict(msg)?;
+                        continue;
+                    }
+                }
 
                 // Do simple check: Is this packet TLS-handshake
                 // Why that?
@@ -43,78 +103,11 @@ pub fn start_sniff() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 // And 0x16 - it is first bytes of tls-handshake
                 if tcp_payload.len() > 5 && tcp_payload[0] == 0x16 {
                     println!("This look like a TLS handshake packet!");
+                    pending = Some((seq, tcp_payload.to_vec()));
 
-                    let mut is_here = 0;
-
-                    for &seq_vec in &sequence_storage {
-                        if seq_vec == seq {
-                            is_here = 1;
-                        }
-                    }
-                    if is_here != 1 {
-                        sequence_storage.push(seq);
-                        msg.set_verdict(Verdict::Drop);
-                        queue.verdict(msg)?;
-                        continue;
-                    } else if is_here == 1 {
-                        
-                    }
-                    // "find-sni" - it is my function.
-                    // You can read her code!
-                    // In a nutshell this function return 2 values:
-                    // Split posicion and damain
-                    if let Some((split_pos, domain)) = find_sni(tcp_payload) {
-                        println!("Found SNI: {} (split at {})", domain, split_pos);
-
-                        // Generate random value of junk data
-                        let mut rng = thread_rng();
-                        let trash = rng.gen_range(10..=30);
-
-                        // Recognize sequence number this packet
-                        let real_seq = tcp_packet.get_sequence();
-
-                        // Create vector that will be populated
-                        // letters "A". There will be our random num of "A"
-                        let junk: Vec<u8> = vec![0x41; trash];
-                        let mut packet1_payload = junk.clone();
-
-                        // We add a piece of real packet
-                        // to our junk (we gum up it in simple words)
-                        packet1_payload.extend_from_slice(&tcp_payload[..split_pos]);
-
-                        // Calculate sequence number for our false packet
-                        let packet1_seq = real_seq.wrapping_sub(trash as u32);
-
-                        let packet2_payload = &tcp_payload[split_pos..];
-
-                        let packet2_seq = real_seq + split_pos as u32;
-
-                        println!(
-                            "Packet 1: seq={}, len={}",
-                            packet1_seq,
-                            packet1_payload.len()
-                        );
-                        println!(
-                            "Packet 2: seq={}, len={}",
-                            packet2_seq,
-                            packet2_payload.len()
-                        );
-
-                        let my_ip =
-                            SocketAddrV4::new(ip_packet.get_source(), tcp_packet.get_source());
-                        let dst_ip = SocketAddrV4::new(
-                            ip_packet.get_destination(),
-                            tcp_packet.get_destination(),
-                        );
-                        let ack = tcp_packet.get_acknowledgement();
-
-                        send_fake_ttl(my_ip, dst_ip, packet1_seq, ack, 64, &packet1_payload)?;
-                        send_fake_ttl(my_ip, dst_ip, packet2_seq, ack, 64, packet2_payload)?;
-
-                        msg.set_verdict(Verdict::Drop);
-                        queue.verdict(msg)?;
-                        continue;
-                    }
+                    msg.set_verdict(Verdict::Drop);
+                    queue.verdict(msg)?;
+                    continue;
                 }
             };
         };
